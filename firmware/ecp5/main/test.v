c1807fb64861074c75cce4eac5709b17ed284244
Terence Chan Zun Mun <zunmun@gmail.com>
2025-05-09 12:52:32 +0800

    feat: uart sample code

--- firmware/ecp5/main/test.v
+++ firmware/ecp5/main/test.v
@@ -9,6 +9,42 @@ module top(input clk, input [4:0] btn, output [7:0] led, output [7:0] interconne
         end
     end
 
+    /// UART ////////////////////////////////////////////////////////////
+    parameter DBITS = 8;
+    parameter UART_FRAME_SIZE = 4;
+    //// UART Setup ///////////////
+    wire  btn_tick;
+    wire [7:0] rec_data, rec_data1;
+    
+    wire [UART_FRAME_SIZE*DBITS-1:0] tx_fifo_out;
+    
+    wire [UART_FRAME_SIZE*DBITS-1:0] rx_out;
+    wire rx_full, rx_empty, rx_tick;
+    // Complete UART Core
+    uart_top 
+        #(
+            .FIFO_IN_SIZE(UART_FRAME_SIZE),
+            .FIFO_OUT_SIZE(UART_FRAME_SIZE),
+            .FIFO_OUT_SIZE_EXP(32)
+        ) 
+        UART_UNIT
+        (
+            .clk_100MHz(clk_100MHz),
+            .write_data(rec_data1),
+            
+            .rx(rx),
+            .tx(tx),
+            
+            .rx_full(rx_full),
+            .rx_empty(rx_empty),
+            .rx_tick(rx_tick),
+            .rx_out(rx_out),
+            
+            .tx_trigger(btn),
+            .tx_in({8'd65, 8'd65, 8'd65, 8'd65})
+        );
+    //////////////////////////////////////////////////////////////
+
     assign led = ~btn[0] ? btn : shooting;
     assign interconnect = {3'b111, btn};
 endmodule
