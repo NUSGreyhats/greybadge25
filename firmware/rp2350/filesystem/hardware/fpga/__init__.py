922b43d976f7b2300d5dda26f8edb0e15d3323b0
Terence Chan Zun Mun <zunmun@gmail.com>
2025-05-11 00:37:36 +0800

    feat: base

---
+++ firmware/rp2350/filesystem/hardware/fpga/__init__.py
@@ -0,0 +1,19 @@
+### ECP5 Programming #####################
+#import hardware.fpga.jtag as jtag
+from hardware.fpga import ecp5p
+from hardware.fpga import ecp5f
+import digitalio, board, time
+
+def upload_bitstream(path):
+    #print("0x%08x" % jtag.idcode())
+    
+    ## Configure Reset Pin
+    jtag_rst = digitalio.DigitalInOut(board.GP20)
+    jtag_rst.direction = digitalio.Direction.OUTPUT
+    jtag_rst.value = False
+    time.sleep(0.1)
+    jtag_rst.value = True
+    
+    ## Upload Bitsream
+    #ecp5f.flash("test.bit")
+    ecp5p.prog("test.bit")
